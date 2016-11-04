84274ae40f4c2a6931a22103d2ee26de23e7acfc
Swapnil Singh <swappp7singh@gmail.com>
2016-11-05 00:55:07 +0530

    tweaked app ran properly

---
+++ tweaked-app/swapnil/main.js
@@ -0,0 +1,6 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var swapnil_module_1 = require('./swapnil.module');
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(swapnil_module_1.SwapnilModule);
+//# sourceMappingURL=main.js.map
