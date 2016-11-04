228fddf0ecaaed820f16726fc32958ed3651595c
Swapnil Singh <swappp7singh@gmail.com>
2016-11-05 00:03:50 +0530

    created app folder which consists app module component and main.ts file

---
+++ angular-quickstart/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:   'my-app',
+    template:   '<h3>Swapnil Singh, Angular Developer</h3>'
+})
+
+export class AppComponent { }
