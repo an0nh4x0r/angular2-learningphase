3aa4c461d028bc0277e0425dfef8dba9aa49b925
Swapnil Singh <swappp7singh@gmail.com>
2016-12-01 12:29:45 +0530

    finished what i wanted to make :D

--- cli_projects/user_app/src/app/user/user.component.ts
+++ cli_projects/user_app/src/app/user/user.component.ts
@@ -10,19 +10,42 @@ export class UserComponent implements OnInit {
   name: string;
   email: string;
   address: address;
+  user_hobbies: string[];
+  showHobbies: boolean;
 
   constructor() {
     this.name = 'Swapnil Singh';
-    this.email = 'swappp7singh@gmail.com'
+    this.email = 'swappp7singh@gmail.com';
+    this.address = {
+      street: '2 Dhanawat Estate',
+      city: 'Gwalior',
+      state: 'Madhya Pradesh'
+    };
+    this.user_hobbies = ['Music', "Coding", 'Soccer', 'Movies'];
+    this.showHobbies = false;
   }
 
   ngOnInit() {
   }
 
+  clearInfo() {
+    this.name = '';
+    this.email = '';
+    this.address = {
+      street: '',
+      city: '',
+      state: ''
+    }
+  }
+
+  hobbies() {
+    this.showHobbies = !this.showHobbies;
+  }
+
 }
 
 interface address {
-  street: '2 Dhanawat Estate';
-  city: 'Gwalior';
-  state: 'Madhya Pradesh';
+  street: string;
+  city: string;
+  state: string;
 }
